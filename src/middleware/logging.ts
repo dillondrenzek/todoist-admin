653b6fc984457cab77680403dea9f1b69f6fd33c
Dillon Drenzek <dillon@drenzek.com>
2023-02-03 22:47:29 -0800

    logging middleware

--- src/middleware/logging.ts
+++ src/middleware/logging.ts
@@ -1,5 +1,8 @@
 import express from 'express';
 
+/**
+ * App logging function
+ */
 export const logRequests: express.RequestHandler = (req, res, next) => {
   console.log(`[${req.method.toUpperCase()}] ${req.path}`);
   next();
